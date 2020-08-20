3d85b64dd8927a166eda01e588b32b1e9ccca726
nikolatriki <kjofte@yahoo.com>
2020-08-20 15:48:29 +0200

    Refactor counter with version three

--- lib/counter.rb
+++ lib/counter.rb
@@ -1,14 +1,27 @@
 (1..100).each do |counter|
-  factors = []
   if (counter % 3).zero? && (counter % 5).zero?
-    factors.push('Ping' + 'Pong')
+	  puts 'Ping' + 'Pong'
   elsif (counter % 3).zero?
-    factors.push('Ping')
+    puts 'Ping'
   elsif (counter % 5).zero?
-    factors.push('Pong')
+    puts 'Pong'
+  else
+    puts counter
   end
-  puts factors.size.positive? ? factors.join('') : counter.to_s
 end
+
+
+# (1..100).each do |counter|
+#   factors = []
+#   if (counter % 3).zero? && (counter % 5).zero?
+#     factors.push('Ping' + 'Pong')
+#   elsif (counter % 3).zero?
+#     factors.push('Ping')
+#   elsif (counter % 5).zero?
+#     factors.push('Pong')
+#   end
+#   puts factors.size.positive? ? factors.join('') : counter.to_s
+# end
 
 # counter = 1
 # while counter <= 100
